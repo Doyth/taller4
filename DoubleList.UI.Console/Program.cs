3f1a3bb1d1376dff93c6c2c9874040f906ffcc79
Doyth <yc8822473@gmail.com>
2025-05-23 22:02:44 -0500

    entrega taller 4

---
+++ DoubleList.UI.Console/Program.cs
@@ -0,0 +1,82 @@
+﻿using DoubleList;
+
+var list = new DoublyLinkedList<string>();
+var opc = "0";
+
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "1":
+            Console.Write("Enter the data to add data: ");
+            var dataToAdd = Console.ReadLine();
+            if (dataToAdd != null)
+            {
+                list.InsertInOrder(dataToAdd);
+            }
+            break;
+        case "2":
+            Console.WriteLine(list.GetForward());
+            break;
+        case "3":
+            Console.WriteLine(list.GetBackward());
+            break;
+        case "4":
+            Console.Write("Enter the data to add data: ");
+            list.ReverseList();
+            break;
+        case "5":
+            Console.Write("Modas: ");
+            Console.WriteLine(list.Moda());
+            break;
+        case "6":
+            Console.WriteLine("Graphic");
+            Console.WriteLine(list.Graphic());
+            break;
+        case "7":
+            Console.Write("Enter the data to check if exists: ");
+            var dataToCheck = Console.ReadLine();
+            if (dataToCheck != null)
+            {
+                Console.WriteLine(list.Exists(dataToCheck));
+            }
+            break;
+        case "8":
+            Console.Write("Enter the data to remove: ");
+            var remove = Console.ReadLine();
+            if (remove != null)
+            {
+                list.Remove(remove);
+                Console.WriteLine("Item removed.");
+            }
+            break;
+        case "9":
+            Console.Write("Enter the data to remove: ");
+            var remove_all = Console.ReadLine();
+            if (remove_all != null)
+            {
+                list.RemoveAllOcurrences(remove_all);
+                Console.WriteLine("Item removed.");
+            }
+            break;
+    }
+}
+while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Show list forward");
+    Console.WriteLine("3. Show list backward");
+    Console.WriteLine("4. Order list backwards (reverse list)");
+    Console.WriteLine("5. Modas");
+    Console.WriteLine("6. Graphic the list");
+    Console.WriteLine("7. Exists");
+    Console.WriteLine("8. Remove One Ocurrence");
+    Console.WriteLine("9. Remove All Ocurrences");
+
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine() ?? "0";
+}
---
+++ DoubleList/DoublyLinkedList.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace DoubleList;
+
+public class DoublyLinkedList<T>
+{
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+    private bool _upward;
+
+    public DoublyLinkedList()
+    {
+        _tail = null;
+        _head = null;
+        _upward = true;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null) // Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+    }
+
+    public void InsertInOrder(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null) // Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            var current = _head;
+            if (_upward)
+            {   // search for a node where Current.data is less than data
+                while (current != null && current.Data.ToString().CompareTo(data) < 0)
+                {
+                    current = current.Next;
+                }
+
+            }
+            else
+            {   // search for a node where Current.data is greater than data
+                while (current != null && current.Data.ToString().CompareTo(data) > 0)
+                {
+                    current = current.Next;
+                }
+
+            }
+
+            if (current == null)
+            {
+                // Insert at the end
+                InsertAtEnd(data);
+            }
+            else if (current.Prev == null)
+            {
+                // Insert at the beginning
+                InsertAtBeginning(data);
+            }
+            else
+            {
+                // Insert in the previous position
+                current.Prev.Next= newNode;
+                newNode.Prev = current.Prev;
+                newNode.Next = current;
+                current.Prev = newNode;
+
+            }
+        }
+
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_tail == null) // Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+    }
+
+    public string GetForward()
+    {
+        var output = string.Empty;
+        var current = _head;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Next;
+        }
+        return output.Substring(0, output.Length-5);
+    }
+
+    public string GetBackward()
+    {
+        var output = string.Empty;
+        var current = _tail;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Prev;
+        }
+        return output.Substring(0, output.Length - 5);
+    }
+
+    public void Remove(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else
+                {
+                    _head = current.Next; // Remove head
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else
+                {
+                    _tail = current.Prev; // Remove tail
+                }
+
+                break;
+            }
+            current = current.Next;
+        }
+    }
+    public void ReverseList()
+    {
+        (_tail, _head) = (_head, _tail);
+        var current = _head;
+        while (current != null)
+        {
+            (current.Next, current.Prev) = (current.Prev, current.Next);
+            current = current.Next;
+        }
+        _upward = !_upward;
+    }
+
+    public string Graphic()
+    {
+        var current = _head;
+        Dictionary<string, int> counter = new Dictionary<string, int>();
+
+        while (current != null)
+        {
+            try {
+                counter[current.Data.ToString()] += 1;
+            
+            }
+            catch {
+                counter.Add(current.Data.ToString(), 1);
+            }
+            current = current.Next;
+        }
+        var graph = "";
+
+        foreach (KeyValuePair<string, int> kvp in counter)
+        {
+            var val = String.Empty;
+            for (global::System.Int32 i = 0; i < kvp.Value; i++)
+            {
+                val += "*";
+            }
+            graph += "\n" + kvp.Key + val;
+
+        }
+
+        return graph;
+    }
+
+        public string Moda()
+    {
+        var current = _head;
+        Dictionary<string, int> counter = new Dictionary<string, int>();
+
+        while (current != null)
+        {
+            try {
+                counter[current.Data.ToString()] += 1;
+            
+            }
+            catch {
+                counter.Add(current.Data.ToString(), 1);
+            }
+            current = current.Next;
+        }
+        var max = counter.Values.Max();
+        string results = "";
+
+        foreach (KeyValuePair<string, int> kvp in counter)
+        {
+            if (kvp.Value == max)
+            {
+                results += kvp.Key + "-";
+            }
+        }
+        return results;
+    }
+    public bool Exists(T data)
+    {
+        var exists = false;
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data.Equals(data))
+            {
+                exists = true;
+                break;
+            }
+            current = current.Next;
+        }
+        return exists;
+    }
+
+
+    public void RemoveAllOcurrences(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else
+                {
+                    _head = current.Next; // Remove head
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else
+                {
+                    _tail = current.Prev; // Remove tail
+                }
+            }
+            current = current.Next;
+        }
+    }
+
+}
